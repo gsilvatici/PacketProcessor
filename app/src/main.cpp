7732cb22dae279eb6665050480cd192058a5b0e5
Gabriel <gsilvati@itba.edu.ar>
2021-12-23 21:45:17 +0000

    Fix cmake static linking

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,9 +2,11 @@
 
 using namespace pp;
 
+#ifndef TESTING
 int main(int argc, char **argv)
 {
     PacketProcessorInitializer initializer;
     initializer.run(argc, argv);
     return 0;
 }
+#endif
--- test/src/PacketProcessorTest.cpp
+++ test/src/PacketProcessorTest.cpp
@@ -1,5 +1,5 @@
-#include "../../app/include/pp/PacketProcessor.h"
-// #include "pp/PacketProcessor.h"
+// #include "../../app/include/pp/PacketProcessor.h"
+#include "pp/PacketProcessor.h"
 #include "gtest/gtest.h"
 
 using namespace pp;
